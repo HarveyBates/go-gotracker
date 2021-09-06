3df6d6436551ecf9cc7b6f18ed2285e9f32adefe
HarveyBates <phenobottle@gmail.com>
2021-09-06 23:10:32 +1000

    idk what im doing with docker

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-echarts/go-echarts/v2 v2.2.4
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.3.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,16 +1,60 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
+	"log"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type TestValues struct {
+	Name string `json:"name"`
+	DOB string `json:"date_of_birth"`
+}
+
+func SqlConnect(){
+	fmt.Println("Attempting to connect to MySQl server...")
+
+	db, err := sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3001)/activities")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.SetConnMaxLifetime(1000)
+
+	defer db.Close()
+
+	fmt.Println("Creating table in db...")
+
+	create, err := db.Query("CREATE TABLE [IF NOT EXISTS] test ( ID int, NAME varchar(255) )")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer create.Close()
+
+	fmt.Println("Inserting values in table...")
+
+	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer insert.Close()
+}
 
 
 func main() {
 
-	refreshToken := GetRefreshToken()
+	SqlConnect()
 
-	distanceArr, wattsArr := GetWatts("5901981172", refreshToken)
 
-	MakeChart(distanceArr, wattsArr, "Workout #1", "2 x 20 min @ 240 Watts")
+//	refreshToken := GetRefreshToken()
+
+//	distanceArr, wattsArr := GetWatts("5901981172", refreshToken)
+
+//	MakeChart(distanceArr, wattsArr, "Workout #1", "2 x 20 min @ 240 Watts")
 }
