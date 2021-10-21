874cbcd17d0f797ced04b553ef41bbbcfb575062
HarveyBates <phenobottle@gmail.com>
2021-10-21 22:27:24 +1100

    got nowhere

--- render/src/views/activities.js
+++ render/src/views/activities.js
@@ -10,6 +10,7 @@ class Activity extends React.Component {
 		super();
 		this.state = {
 			activity: [],
+			stream: "",
 			activity_name: "", 
 			activity_id: "",
 			start_time: "",
@@ -26,31 +27,32 @@ class Activity extends React.Component {
 				"Accept": "application/json",
 				"Content-Type": "application/json"}}, {mode: 'no-cors'});
 			var data = await response.json();
-			this.setState({ activity: data,
-							activity_name: data.activity_name, 
-							activity_id: data.activity_id,
-							start_time: data.start_time,
-							end_time: data.end_time,
-							sport: data.sport});
 
 			// Influx db query
 			const inTok = process.env.REACT_APP_INFLUX_DB;
 			const client = new InfluxDB({url: "http://localhost:8086", token: inTok}).getQueryApi("user");
-			const query = `from(bucket: "records") |> range(start: time(v: ${this.state.start_time}), stop: time(v: ${this.state.end_time})) |> filter(fn: (r) => r["_measurement"] == "${this.state.activity_name}") |> filter(fn: (r) => r["_field"] == "speed")`
-			console.log(query);
-		client.queryRows(query, {
-		  next(row, tableMeta) {
-			const o = tableMeta.toObject(row)
-			  console.log(`${o._time} ${o._field}=${o._value}`);
-		  },
-		  error(error) {
-			console.error(error)
-		  },
-		  complete() {
-			console.log('\\nFinished SUCCESS')
-		  },
-		})
+			const query = `from(bucket: "records") |> range(start: time(v: ${data.start_time}), stop: time(v: ${data.end_time})) |> filter(fn: (r) => r["_measurement"] == "${data.activity_name}") |> aggregateWindow(every: 20s, fn: mean)`
 
+			let influxResponse = [];
+			client.queryRows(query, {
+				next(row, tableMeta) {
+					let record = tableMeta.toObject(row)
+					influxResponse.push(record);
+				},
+				error(error) {
+					console.error(error)
+				},
+				complete() {
+					console.log('\nFinished SUCCESS')
+				},
+			})
+			this.setState({	activity: data, 
+							stream: influxResponse,
+							activity_name: data.activity_name, 
+							activity_id: data.activity_id,
+							start_time: data.start_time,
+							end_time: data.end_time,
+							sport: data.sport });
 		} catch (error) {
 			console.log(error);
 		}
@@ -59,16 +61,137 @@ class Activity extends React.Component {
 
 	render() {
 		if (this.state.activity.length === 0) {
+			console.log("Empty arr")
 			return (
 			<div>
 				<p> Loading chart... </p>
 			</div>);
 		}
 		else{
+			console.log(this.state.activity)
+			console.log(this.state.stream[0])
+			
+			const options = {
+				title: {
+					text: this.state.activity_name,
+					subtext: moment(this.state.start_time).format("DD-MM-YYYY hh:mm A"),
+					left: 100,
+				},
+				xAxis: {
+					name: "Distance (km)",
+					nameLocation: 'center',
+					nameGap: -15,
+					//max: Math.floor(distance.slice(-1)[0]) / 1000,
+					type: 'value',
+				},
+				yAxis: [
+					{
+						type: 'value',
+						position: 'left'
+					},
+					{
+						type: 'value',
+						position: 'right',
+						name: "Altitude (m)"
+					},
+				],
+				tooltip: {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'cross'
+					}
+				},
+				toolbox: {
+					show: true,
+					right: 100,
+					feature: {
+						saveAsImage: {},
+						dataZoom: {},
+					}
+				},
+				dataZoom: [
+					{
+						show: true,
+						realtime: true,
+						start: 0,
+						end: 100,
+					}
+				],
+				series: [
+					{
+						name: "Heartrate (bpm)",
+						color: 'rgba(255, 80, 80, 1)',
+						type: 'line',
+						symbol: 'none',
+						data: []
+					},
+					{
+						name: "Power Zones",
+						type: "line",
+						data: [],
+						markArea: {
+							label: {
+								show: true,
+								position: 'inside',
+								color: "#000",
+							},
+							data: [
+								[{
+									name: "Zone 1",
+									itemStyle: {
+										color: 'rgba(85, 155, 255, 0.2)'
+									},
+										yAxis: 0
+									},
+									{
+										yAxis: 145
+									}
+								],
+								[{
+									name: "Zone 2",
+									itemStyle: {
+										color: 'rgba(125, 220, 80, 0.2)'
+									},
+										yAxis: 145
+									},
+									{
+										yAxis: 200
+									}
+								],
+								[{
+									name: "Zone 3",
+									itemStyle: {
+										color: 'rgba(200, 165, 80, 0.2)'
+									},
+										yAxis: 200
+									},
+									{
+										yAxis: 261
+									}
+								],
+								[{
+									name: "Zone 4",
+									itemStyle: {
+										color: 'rgba(255, 55, 80, 0.2)'
+									},
+										yAxis: 261
+									},
+									{
+										yAxis: 2000
+									}
+								]
+							]
+						}
+					}
+				]
+			};
 			return (
 				<div className="main-container">
 					<div className="chart-map">
 						<div className="chart">
+							<ReactECharts option={options} 
+								theme={'macarons'} 
+								style={{height: '100%', width: '100%'}}/>
 						</div>
 					</div>
 					<table className="activity-table">
